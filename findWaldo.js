b2bba71b698786bff1c5d8c890ab963c09339e04
PreranaShrestha <prerana.sh@gmail.com>
2018-01-18 19:32:30 +0000

    implement forEach loop instead of for loop

--- findWaldo.js
+++ findWaldo.js
@@ -1,9 +1,10 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(item, ind) {
+    if(item === "Waldo") {
+      found(ind);
     }
-  }
+  });
+
 }
 
 function actionWhenFound(i) {
